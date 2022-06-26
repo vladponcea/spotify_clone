e7bfecd62b70f047bc2dbe6a6da044c78294dcda
vladponcea <vladponcea@icloud.com>
2022-06-26 20:42:58 +0300

    added new releases to homepage

---
+++ spotify_clone/ViewModels/NewReleasesCellViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  NewReleasesCellViewModel.swift
+//  spotify_clone
+//
+//  Created by Vladut Mihai Poncea on 26.06.2022.
+//
+
+import Foundation
+
+struct NewReleasesCellViewModel {
+    let name: String
+    let artworkURL: URL?
+    let numberOfTracks: Int
+    let artistName: String
+}
