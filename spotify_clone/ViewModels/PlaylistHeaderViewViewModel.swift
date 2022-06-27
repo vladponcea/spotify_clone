c6fc0d9828639d2862e2fcab557f1e9e01e37285
vladponcea <vladponcea@icloud.com>
2022-06-27 17:16:25 +0300

    playlist detailed view

---
+++ spotify_clone/ViewModels/PlaylistHeaderViewViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  PlaylistHeaderViewViewModel.swift
+//  spotify_clone
+//
+//  Created by Vladut Mihai Poncea on 27.06.2022.
+//
+
+import Foundation
+
+struct PlaylistHeaderViewViewModel {
+    let name: String?
+    let ownerName: String?
+    let description: String?
+    let artworkURL: URL?
+}
